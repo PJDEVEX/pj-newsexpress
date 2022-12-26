f77edfdfe1660ce8e53b261f67d5f388f7acb6df
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2022-12-26 23:36:21 +0000

    Create interactive nav vertical nav section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,12 @@
 // Getting access to html elements
 // Accessing menu-toggle class
 let menu_toggle = document.querySelector(".menu-toggle");
+let menu_section = document.querySelector(".menu-section");
 
-// Create event listner
+
+// Create event listners
 menu_toggle.addEventListener("click", () => {
     menu_toggle.classList.toggle("is-active");
+    // event listener for navigation section
+    menu_section.classList.toggle("is-active"); 
 })
